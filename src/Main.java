83f556ac0338e41f161a76c810dea5c6256d70cc
Jean-Jensen <jeanjensen911@gmail.com>
2023-11-22 16:49:08 +0700

    added a useless method

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public void doThing(){
+        //sgokjndgjdjkgfndgkn
     }
 }
